ee24e102762f61313d73d2c87596482076b720f1
MSpreij <mspreij@gmail.com>
2020-12-11 17:53:40 +0100

    10b - that didn't take over a day at all!

--- 10.php
+++ 10.php
@@ -4,15 +4,24 @@
 $numbers = explode("\n", trim(file_get_contents('10.txt')));
 
 // 10a
+// sort($numbers);
+// array_unshift($numbers, 0); // outlet
+// $diff = [1=>0, 0, 0];
+// for ($i=0; $i < count($numbers)-1; $i++) {
+//     $diff[$numbers[$i+1] - $numbers[$i]]++;
+// }
+// $diff[3]++; // device
+// echo $diff[1] * $diff[3];
+
+// 10b
 sort($numbers);
 array_unshift($numbers, 0); // outlet
-$diff = [1=>0, 0, 0];
-for ($i=0; $i < count($numbers)-1; $i++) {
-    $diff[$numbers[$i+1] - $numbers[$i]]++;
+$configs_at[0] = 1;
+for ($i=1; $i < count($numbers); $i++) {
+    $num = $numbers[$i];
+    $configs_at[$num] = 0;
+    for ($j=1; $j <= 3; $j++) $configs_at[$num] += $configs_at[$num-$j] ?? 0;
 }
-$diff[3]++; // device
-echo $diff[1] * $diff[3];
-
-
+echo end($configs_at);
 
 ?>
